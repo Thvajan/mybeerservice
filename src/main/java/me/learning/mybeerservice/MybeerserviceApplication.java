45752bd1634157de69eaceaffc2cfaa4a96d8fb3
srive <srive@DESKTOP-O3BL95U>
2020-11-09 20:07:44 +0530

    Project initial Commit

---
+++ src/main/java/me/learning/mybeerservice/MybeerserviceApplication.java
@@ -0,0 +1,13 @@
+package me.learning.mybeerservice;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class MybeerserviceApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(MybeerserviceApplication.class, args);
+	}
+
+}
---
+++ src/test/java/me/learning/mybeerservice/MybeerserviceApplicationTests.java
@@ -0,0 +1,13 @@
+package me.learning.mybeerservice;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class MybeerserviceApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
